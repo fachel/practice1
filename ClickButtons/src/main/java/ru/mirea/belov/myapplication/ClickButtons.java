fc286be2004b3672900a23181edfc9374c31998f
Fachel <belov.dmitry.32@yandex.ru>
2019-10-20 14:27:38 +0300

    first commit

---
+++ ClickButtons/src/main/java/ru/mirea/belov/myapplication/ClickButtons.java
@@ -0,0 +1,44 @@
+package ru.mirea.belov.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ClickButtons extends AppCompatActivity {
+
+    TextView tvOut;
+    Button btnOk;
+    Button btnCancel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_click_buttons);
+
+        tvOut = (TextView) findViewById(R.id.tvOut);
+        btnCancel = (Button) findViewById(R.id.btnCancel);
+        btnOk = (Button) findViewById(R.id.btnOk);
+
+        View.OnClickListener oclBtnOk = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvOut.setText("Нажата кнопка ОК");
+            }
+        };
+
+        btnOk.setOnClickListener(oclBtnOk);
+
+        View.OnClickListener oclBtnCancel = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvOut.setText("Нажата кнопка Cancel");
+            }
+        };
+
+        btnCancel.setOnClickListener(oclBtnCancel);
+    }
+}
